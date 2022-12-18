027be942ba088dcdb16ec215611e13bda3299e40
DuyenTM <tmdmduyen@gmail.com>
2022-12-18 17:37:33 +0700

    fix bug vercel

--- app.js
+++ app.js
@@ -10,4 +10,25 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use("/api/v1", indexRouter);
 
+app.use(express.static(__dirname + "/src"));
+
+app.get("/home", function (req, res) {
+  res.sendFile(path.join(__dirname, "./src/page/home.html"));
+});
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "./src/page/login.html"));
+});
+app.get("/visit", function (req, res) {
+  res.sendFile(path.join(__dirname, "./src/page/visit.html"));
+});
+app.get("/contact", function (req, res) {
+  res.sendFile(path.join(__dirname, "./src/page/contact.html"));
+});
+app.get("/update", function (req, res) {
+  res.sendFile(path.join(__dirname, "./src/page/update.html"));
+});
+app.get("/3dMap", function (req, res) {
+  res.sendFile(path.join(__dirname, "./src/page/render.html"));
+});
+
 module.exports = app;
